ff2b9b83bf063c3472c764fadd926cdfc9f21027
nog <nog.guitar@gmail.com>
2020-01-21 20:54:14 -0300

    Criando componente Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SharedService } from './services/shared.service';
+import { UserService } from './services/user.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,10 +26,12 @@ import { routes } from './app.routes';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
+    HttpClientModule,
     routes
   ],
-  providers: [],
+  providers: [UserService, SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
